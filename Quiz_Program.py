c13590cc6d72d1bbdcddb09ef46e2fa5c06fb04a
Tim-dejong14 <timothy.dejong@collingwood.school.nz>
2025-05-29 13:18:23 +1200

    changed stuff

--- Quiz_Program.py
+++ Quiz_Program.py
@@ -1,5 +1,5 @@
 total_score = 0
-valid_answers = ["a", "b", "c","d"]
+valid_answers = ["a", "b", "c", "d"]
 
 print("Hello, welcome to the quiz!")
 print("Todays quiz is on general knowledge, there will be 5 MULTI choice questions to answer!\n")
@@ -13,29 +13,20 @@ questions = [["What is the capital of France?", "A: Berlin", "B: Madrid", "C: Ro
              ]
 answers = ["d", "a", "b", "c", "a"]
 
-loop_num = 0 #adds a loopnumber so that it is easier to check what question we are on
-while True:
-    for question in questions:
-        loop_num += 1 
-        for line in question:
-            print(line)
-        while True: # while loop to make try loop repeat until we get it right
-            try:
-                answer = input("Enter your answer: ").lower() #.lower makes it from an uppcase to a lowrcase so it doesnt matter what you use.
+for i, question in enumerate(questions): # creates a index for which number the question is on as the i variable and puts the question as the questions details
+    for line in question:
+        print(line)
+    while True:
+        answer = input("Enter your answer: ").lower()
+        if answer in valid_answers:
+            print("question answered\n")
+            if answer == answers[i]:
+                total_score += 1
+            break
+        else:
+            print("Enter a valid answer")
 
-                if answer in valid_answers: # in checks if the answer is in the list of valid answers
-                    print("question answered\n")
-                    if answer == answers[loop_num - 1]: #we minus 1 because off how lists are stored with the first value being at point 0 in the list
-                        total_score += 1 #the answers[loop_num] means that if the answer that we give is the same number of answer as the number of loop or question it will increase our score
-                    break
-                else:
-                    print("Enter a valid answer")
-                    
-            except:
-                pass
-    if loop_num == len(questions): # checks how many questions there are and only exits the loop when all questions are answered
-        print("Quiz over")
-        break
+print("Quiz over")
 
 if total_score == 0:
     print(f"You failed the quiz and got {total_score}/{len(questions)} correct...")#feedback if you fail the quiz 
@@ -43,6 +34,3 @@ elif total_score == len(questions):
     print (f"You got {len(questions)}/{len(questions)} questions correct and have won the quiz! congrats~!") #len questions is to say that we have the max number of something so using it here is saying we have 5/5 questions answered.
 else:
     print(f"Your score is {total_score}/{len(questions)}")#feedback if you get a question that is not 100% or 0% correct
-
-
-
